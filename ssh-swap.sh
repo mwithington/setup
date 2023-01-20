d9aaebcf6ba0524eb297e95b9c5e26739189ae3a
Mike Withington <mwithington1995+simplisafe@gmail.com>
2023-01-20 15:27:30 -0500

    updates

--- ssh-swap.sh
+++ ssh-swap.sh
@@ -1,3 +1,9 @@
+#! /bin/bash
 echo "~~ Swapping SSH ~~"
 
+mv ~/.ssh/config ~/.ssh/config.alt.temp
+mv ~/.ssh/config.alt ~/.ssh/config
+mv ~/.ssh/config.alt.temp ~/.ssh/config.alt
+
+cat ~/.ssh/config
 echo "~~ Swapping Complete ~~"
